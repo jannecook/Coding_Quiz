81cfb044973c83640c8104c9de63d672a724b1ec
jannecook <135638400+jannecook@users.noreply.github.com>
2023-09-23 14:52:25 -0700

    General framework for basic code and files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,31 @@
 //Timed coding quiz
+var timerEl = document.querySelector(".timer");
+//var mainEl = document.getElementByID("main");
+var secondsLeft = 60
+
+function Timer() {
+    var timerInterval = setInterval(function () {
+        //Decrements seconds left by one
+        secondsLeft--;
+        //NEED TO ADD decrement for wrong answers
+        timerEl.textContent = secondsLeft;
+
+        if(secondsLeft <=0 || ADD CODE FOR NO QUESTIONS LEFT) {
+            //Stops set Interval function
+            clearInterval(timerIntverval);
+            //Call message function
+            sendmessage();
+        }
+    //Interval given in milliseconds
+    }, 1000);
+    }
+function sendMessage() {
+    //select location for message
+    //e.g. timeEl.textContent = "Game Over."
+}
+//Run timer function.
+ADD EVENT LISTENER FOR BUTTON CLICK HERE setTimeout();
+
 //Multiple choice questions
 //Update HTML and CSS elements through JavaScript ConvolverNode
 //Store high scores
